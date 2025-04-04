0abda08b58f0cfb126cc91a32f17b60b7e0f1c4b
Nicat-Mehtizade <nicat.mehdizadeh@gmail.com>
2025-04-04 14:29:48 +0400

    Feat | Fe | (backend) Roots configured in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,24 @@
-const express=require("express")
-const dotenv=require("dotenv")
-const cors=require("cors")
-const app=express()
-const connectDB=require("./config/db")
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+const connectDB = require("./config/db");
+const userRouter = require("./routes/userRouter");
+const commentRouter = require("./routes/commentRouter");
+const activityRouter = require("./routes/activityRouter");
+connectDB();
 
-connectDB()
+dotenv.config();
 
-dotenv.config()
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.use("/api/users", userRouter);
+app.use("/api/comments", commentRouter);
+app.use("/api/activity", activityRouter);
 
-
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-  })
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
